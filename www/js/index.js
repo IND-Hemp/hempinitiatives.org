850ef2eca5ef51422f07d34f417f207f008e6c34
Tom Rankin <tom@tomrankin.net>
2024-10-13 15:48:24 -0500

    mobile nav

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,15 @@
-/* script.js */
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('.nav-list a');
 
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
+    nav.classList.toggle('show');
     hamburger.classList.toggle('toggle');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show');
+        hamburger.classList.remove('toggle');
+    });
 });
